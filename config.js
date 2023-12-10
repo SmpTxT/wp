daf0bc4f8a2217d2a5eb5463c7bd19e441a2be4b
SmpTxT <67049399+SmpTxT@users.noreply.github.com>
2023-12-10 14:46:10 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap:{
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=Moscow&units=metric",
+        SECRET_KEY: "95b5f9f6e732cfc2abd36b9ff9450e8c"
+    }
+}
+
+module.exports = constants
